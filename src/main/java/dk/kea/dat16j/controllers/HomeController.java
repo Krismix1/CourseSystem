d828513822cf554c0e735277cfd0d9f6addd9163
Epharas <cbetivu@gmail.com>
2017-11-20 11:06:09 +0100

    Add nav bars for home page and login page

--- src/main/java/dk/kea/dat16j/controllers/HomeController.java
+++ src/main/java/dk/kea/dat16j/controllers/HomeController.java
@@ -2,11 +2,17 @@ package dk.kea.dat16j.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Chris on 13-Nov-17.
@@ -24,5 +30,16 @@ public class HomeController {
         } else {
             return new ModelAndView("homepage/home-signed-in");
         }
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        // This is not good for unit testing, using the SecurityContextHolder.getContext().getAuthentication()
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+//        return "redirect:/";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 }
