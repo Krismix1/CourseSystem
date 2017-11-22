7518147a220256a21a388ef984ec48d6551f1bc6
Epharas <cbetivu@gmail.com>
2017-11-22 11:25:07 +0100

    Random stuff

--- src/test/java/dk/kea/dat16j/CoursesSystemApplicationTests.java
+++ src/test/java/dk/kea/dat16j/CoursesSystemApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static junit.framework.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,8 @@ public class CoursesSystemApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		RestTemplate t = new RestTemplate();
+//		assertTrue(t.getForObject("http://localhost:8080/", String.class).contains("This page will be displayed for users that are not signed in"));
 	}
 
 }
